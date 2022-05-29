8697f6344e85221217e9ac07c13a7f5baf5cd4d6
Atomic <sasha.belyakov02@gmail.com>
2022-05-30 00:22:25 +0300

    Design changes

--- CafeApplication/Properties/Resources.Designer.cs
+++ CafeApplication/Properties/Resources.Designer.cs
@@ -91,6 +91,16 @@ namespace CafeApplication.Properties {
         }
         
         /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Icon, аналогичного (Значок).
+        /// </summary>
+        public static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
         public static System.Drawing.Bitmap product {
--- CafeWebApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CafeWebApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
